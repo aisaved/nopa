767b4dfb91a9f748a5ebc25afe3b32ecc30ce163
Devasia Joseph <devasiajoseph@gmail.com>
2015-08-13 15:12:19 +0530

    Added constants for reuse

---
+++ src/accrue/constants.clj
@@ -0,0 +1,10 @@
+(ns accrue.constants)
+
+
+;;Define various stories and keys
+
+(def data-daily "data-daily")
+(def data-monhtly "data-monthly")
+(def almanac-daily "almanac-daily")
+(def almanac-monthly "almanac-monthly")
+(def almanac-weekly "almanac-weekly")
