08556c0dc07963361cabeec397ea14d1c1f7521f
Devasia Joseph <devasiajoseph@gmail.com>
2015-08-16 14:01:09 +0530

    Grouped month weekly data

--- src/accrue/insights/almanac/weekly.clj
+++ src/accrue/insights/almanac/weekly.clj
@@ -90,3 +90,87 @@
   (timbre/info (str "Starting weekly pattern processing for " symbol))
   (let [data (sort-by #(:time %) (data-model/get-history-data symbol "daily"))]
     (transform-weekly-data data)))
+
+;;-----------------------------------------------------------------------------
+
+
+(defn collect-mw-years-data
+  [previous next]
+  (assoc previous :gl-percent (conj (:gl-percent previous) (:gl-percent next))))
+
+(defn reduce-mw-data
+  [each-data]
+  (let [data-group (second each-data)
+        month (first each-data)
+        symbol (:symbol (first data-group))
+        init-structure {:gl-percent [] :symbol symbol :month month}]
+    (reduce collect-mw-years-data init-structure data-group)))
+
+(defn aggregate-mw-data
+  [data]
+  (map reduce-mw-data data))
+
+(defn sort-mw-data [each-data]
+  [(first each-data) (sort-by #(:sort-key %) (second each-data))])
+
+
+(defn sorted-mw-data
+  [data]
+  (map sort-mw-data data))
+
+(defn regroup-mw-data
+  [data]
+  (group-by #(:month %) data))
+
+(defn fetch-relevant-data
+  [each-mw-week-group]
+  (calc/ohlc-group-gl-percent each-mw-week-group))
+
+(defn relevant-data
+  [each-data]
+  (let [mw-week-group (second each-data)
+        reference (first (first (second each-data)))
+        years-passed (calc/years-passed reference)
+        month (t/timestamp-to-month (:time reference))]
+    {:symbol (:symbol reference)
+     :gl-percent (map fetch-relevant-data mw-week-group)
+     :month month
+     :years-passed years-passed
+     :sort-key (Integer. (str month years-passed))}))
+
+(defn prepare-mw-data [data]
+  (map relevant-data data))
+
+
+(defn mw-week-groups
+  [each-data]
+  (let [month-group (second each-data)]
+    [(first each-data) (conj (into [] (partition 5 month-group )) (take-last 5 month-group))]))
+
+(defn group-mw-week-data
+  [data]
+  (map mw-week-groups data))
+
+
+(defn group-mw-data
+  [data]
+  (group-by #(str (t/timestamp-to-month (:time %)) "-" (calc/years-passed %)) data))
+
+
+(def transform-mw-data
+  (comp
+   
+   aggregate-mw-data
+   sorted-mw-data
+   regroup-mw-data
+   prepare-mw-data
+   group-mw-week-data
+   group-mw-data))
+
+
+;;mw = month weekly pattern
+(defn process-mw-pattern
+  [symbol]
+  (timbre/info (str "Starting month weekly pattern processing for " symbol))
+  (let [data (sort-by #(:time %) (data-model/get-history-data symbol "daily"))]
+    (transform-mw-data data)))
