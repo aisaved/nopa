b8c55c2cb9803ea5e2e7cba0da7a73bcc1eba287
Devasia Joseph <devasiajoseph@gmail.com>
2015-08-18 14:48:16 +0530

    Weekly process added

--- src/accrue/insights/almanac/process.clj
+++ src/accrue/insights/almanac/process.clj
@@ -5,12 +5,31 @@
                      alts! alts!! timeout]]
             [accrue.insights.almanac.daily :as daily-almanac]
             [accrue.insights.almanac.monthly :as monthly-almanac]
+            [accrue.insights.almanac.weekly :as weekly-almanac]
             [accrue.data.barchart :as barchart]
             [accrue.data.symbols :as symbols]
             [accrue.insights.almanac.log :as log]
             [taoensso.timbre :as timbre]
             [accrue.constants :as c]
             ))
+
+
+
+(defn process-weekly-data
+  [symbol]
+  (if (log/data-processed? c/almanac-weekly symbol)
+    (timbre/info (str "Weekly pattern already computed for " symbol))
+    (do
+      (weekly-almanac/process-weekly-pattern symbol)
+      (log/log-process c/almanac-weekly symbol true))))
+
+(defn process-month-weekly-data
+  [symbol]
+  (if (log/data-processed? c/almanac-month-weekly symbol)
+    (timbre/info (str "Month Weekly pattern already computed for " symbol))
+    (do
+      (weekly-almanac/process-month-weekly-pattern symbol)
+      (log/log-process c/almanac-month-weekly symbol true))))
 
 
 
@@ -44,6 +63,8 @@
       (barchart/fetch-daily-data symbol)
       (log/log-process c/data-daily symbol true)
       (process-monthly-data symbol)
+      (process-weekly-data symbol)
+      (process-month-weekly-data symbol)
       (process-daily-data symbol))))
 
 
